826a75f94130937aa9fc8d3c1534615ccc76563b
WebDeveloper3172 <109694564+WebDeveloper3172@users.noreply.github.com>
2022-10-23 22:43:27 +0300

    Add files via upload

---
+++ popup9/FileJs/popup9.js
@@ -0,0 +1,6 @@
+function togglepopup1() {
+  document.getElementById("popup-1").classList.toggle("active");
+}
+function togglepopup2() {
+  document.getElementById("popup-2").classList.toggle("active");
+}
